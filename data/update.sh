a92d74a2c343760ac98820ea7dcc700074481fc4
Jonas Myrlund <myrlund@gmail.com>
2014-03-05 13:05:34 +0100

    Fixed a couple of bugs in the ingestion scripts.

--- data/update.sh
+++ data/update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-s3cmd get --recursive --skip-existing s3://appearin-kissmetrics kissmetrics
-s3cmd get --force s3://appearin-kissmetrics/* kissmetrics
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+s3cmd get --recursive --skip-existing s3://appearin-kissmetrics $DIR/kissmetrics
+s3cmd get --force s3://appearin-kissmetrics/* $DIR/kissmetrics
+
