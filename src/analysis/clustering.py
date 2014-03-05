b977a73e2f07a871666d230a369f2ec384885747
Jonas Myrlund <myrlund@gmail.com>
2014-03-05 19:32:13 +0100

    Improved clustering approach.

--- src/analysis/clustering.py
+++ src/analysis/clustering.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from sklearn.cluster import KMeans, MeanShift, estimate_bandwidth
+from sklearn.cluster import KMeans, MeanShift, DBSCAN, estimate_bandwidth
 import numpy as np
 
 from analysis import util
@@ -11,39 +11,30 @@ def feature_values_for_person(cursor, person, valid_features):
     return tuple(feature_values[f] for f in valid_features)
 
 def get_data_vectors(cursor, features):
-    
-    # Ensure the feature set is sorted
-    features = sorted(features)
-    
+
     # Get all persons present in user_models for wanted features
     persons = util.get_all_persons(cursor)
-    
+
     # Map each person to value vector for features
     vectors = map(lambda person: feature_values_for_person(cursor, person, features), persons)
-    
+
     return np.array(vectors)
 
-def run(args, connection, features):
-    """Runs clustering routine."""
-    
-    X = get_data_vectors(connection.cursor(), features)
-    
+def cluster(X, n_clusters, features):
     # bandwidth = estimate_bandwidth(X, quantile=0.5, n_samples=5000)
-    # ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    ms = KMeans(n_clusters=5, n_jobs=-1)
-    
-    ms.fit(X)
-    
-    labels = ms.labels_
-    cluster_centers = ms.cluster_centers_
-    
-    labeled_centers = map(lambda c: zip(features, c), cluster_centers)
-    print "Centers:"
-    print "\n".join(map(str, labeled_centers))
-    
+    # clusterer = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    # clusterer = KMeans(n_clusters, n_jobs=1)
+    clusterer = DBSCAN(3.0, 10)
+    clusterer.fit(X)
+
+    labels = clusterer.labels_
+    # cluster_centers = clusterer.cluster_centers_
+
+    # labeled_centers = map(lambda c: zip(features, c), cluster_centers)
+
     labels_unique = np.unique(labels)
     n_clusters_ = len(labels_unique)
-    
+
     import pylab as pl
     from itertools import cycle
 
@@ -53,8 +44,28 @@ def run(args, connection, features):
     colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
     for k, col in zip(range(n_clusters_), colors):
         my_members = labels == k
-        cluster_center = cluster_centers[k]
+        # cluster_center = cluster_centers[k]
         pl.plot(X[my_members, 0], X[my_members, 1], col + '.')
-        pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
-    pl.title('Estimated number of clusters: %d' % n_clusters_)
-    pl.show()
+        # pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
+    pl.title('Number of clusters: %d' % n_clusters_)
+    fname = 'images/%s-%d-features-%d-clusters.png' % (clusterer.__class__.__name__.lower(), len(features), n_clusters_)
+    pl.savefig(fname)
+    print " - wrote cluster visualization to %s." % fname
+
+    # return labeled_centers
+
+def run(args, connection, features):
+    """Runs clustering routine."""
+
+    # Ensure the feature set is sorted
+    features = sorted(features)
+
+    # Grab user model vectors
+    X = get_data_vectors(connection.cursor(), features)
+
+    # Loop through requested cluster cardinalities
+    # for n_clusters in xrange(2, 6+1):
+    cluster(X, n_clusters=None, features=features)
+    # print "Centers for n = %d:" % n_clusters
+    # print "\n".join(map(str, centers))
+
--- src/main.py
+++ src/main.py
@@ -11,27 +11,30 @@ def create_table(conn):
     conn.commit()
 
 # Feature metric functions
-from metrics.simple import rooms_used, rooms_claimed, roomnames_generated, frequency_last_month
+from metrics.simple import rooms_used, rooms_claimed, roomnames_generated, frequency_last_month, chat_message_sent
 from metrics.graph import conversation_partners_per_person, conversations_per_person
 
-FEATURES = (conversation_partners_per_person, conversations_per_person, rooms_used) # , rooms_claimed, roomnames_generated, frequency_last_month
+FEATURES = (conversation_partners_per_person, conversations_per_person,  rooms_used, rooms_claimed, roomnames_generated, frequency_last_month, chat_message_sent)
 
-def build_feature_set(args, connection):
+def build_feature_set(args, connection, feature_names):
     """Clears and rebuilds selected features from various general data sources."""
-    
+
     if args.create:
         create_table(connection)
-    
-    for feature_fn in FEATURES:
-        name = feature_fn.__name__
-        
+
+    feature_fns = dict(zip(map(lambda fn: fn.__name__, FEATURES), FEATURES))
+    print feature_fns
+
+    for name in feature_names:
+        feature_fn = feature_fns[name]
+
         if args.reset or raw_input("Rebuild feature '%s'? [yN] " % name) in ("y", "Y"):
-        
+
             print name
-            
+
             print "  - clearing old values"
             util.clear_feature_values(connection, name)
-            
+
             print "  - extracting feature values: ",
             for batch in feature_fn(connection.cursor(), batch_size=args.batch_size):
                 util.insert_feature_values(connection, name, batch)
@@ -39,25 +42,25 @@ def build_feature_set(args, connection):
                 sys.stdout.flush()
             print
 
-def analyze_feature_set(args, connection):
+def analyze_feature_set(args, connection, feature_names):
     """Analyzes and plots various standard metrics for each feature."""
-    
-    for feature_fn in FEATURES:
-        name = feature_fn.__name__
-        
+
+    for name in feature_names:
         values = util.get_feature_values(connection, name)
-        
+
         stats.analyze_feature(connection, name, values)
-        
+
         if args.visualize:
             visualize.plot_feature(connection, name, values)
 
 if __name__ == '__main__':
     from util import load_config, connect_db
     CONFIG = load_config()
-    
+
     connection = connect_db(CONFIG)
-    
+
+    feature_names = map(lambda f: f.__name__, FEATURES)
+
     import argparse
     parser = argparse.ArgumentParser(description="Parses sentences.")
     parser.add_argument('-b', '--batch-size', help="batch size", type=int, default=500)
@@ -66,19 +69,23 @@ if __name__ == '__main__':
     parser.add_argument('--analyze', help="analyze features", action='store_true', default=False)
     parser.add_argument('--build', help="rebuild feature index", action='store_true', default=False)
     parser.add_argument('--visualize', help="visualize features upon analysis", action='store_true', default=False)
+    parser.add_argument('features', help="override feature selection", nargs=argparse.REMAINDER, choices=feature_names)
     args = parser.parse_args()
-    
+
+    selected_feature_names = args.features or feature_names
+
+    print "~ Using %d features" % len(selected_feature_names)
+
     if args.build:
         print "~ Building feature set"
-        build_feature_set(args, connection)
+        build_feature_set(args, connection, selected_feature_names)
         print
-    
+
     if args.analyze:
         print "~ Feature set analysis"
-        analyze_feature_set(args, connection)
+        analyze_feature_set(args, connection, selected_feature_names)
         print
-    
+
     print "~ Perform clustering routine"
-    feature_names = map(lambda f: f.__name__, FEATURES)
-    clustering.run(args, connection, feature_names)
+    clustering.run(args, connection, selected_feature_names)
     print
