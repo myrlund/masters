56ecd8220b84179310aed840116efa89d305cbd2
Jonas Myrlund <myrlund@gmail.com>
2014-03-18 13:30:15 +0100

    Updated log and pdf.

--- report/write_log.sh
+++ report/write_log.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-git log --pretty=format:'\item[%ad] %s%d \\' --date=short --no-merges -- "$DIR/*.tex" > $DIR/log.txt
+git log --pretty=format:'\item[%ad] %s%d \\' --date=short --no-merges -- "$DIR/*.tex" | uniq > $DIR/log.txt
 echo >> $DIR/log.txt
